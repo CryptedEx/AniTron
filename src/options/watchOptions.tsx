c0eb89c4a57931938240b8b1a7953636a5a6f8c8
Awe <106132059+Awe03@users.noreply.github.com>
2022-12-19 20:24:33 +0530

    Delete src directory

--- src/options/watchOptions.tsx
+++
@@ -1,6 +0,0 @@
-export const providerOptions = [
-    {value: 'gogoanime', label: 'GogoStream'},
-    {value: 'crunchyroll', label: 'CrunchyRoll'},
-    {value: 'zoro', label: 'Zoro'},
-    //{value: 'animepahe', label: 'AnimePahe'},
-]
--- src/pages/api/status.ts
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-const Status = (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Cache-Control', 's-maxage=86400');
-  res.status(200).json({ message: 'Status: OK' })
-}
-
-export default Status;
