9dcf22e54a97929727fb1e0116c106e804cee191
Awe <106132059+Awe03@users.noreply.github.com>
2022-12-24 12:09:12 +0530

    Update status.ts

--- src/pages/api/status.ts
+++ src/pages/api/status.ts
@@ -1,9 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next'
 
 const Status = (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Cache-Control', 's-maxage=86400');
-  res.status(200).json({ message: 'Status: OK' })
+    res.setHeader('Cache-Control', 's-maxage=86400');
+    let uptime: number = process.uptime();
+
+    res.status(200).json({message: 'Status: OK', uptime: `${Math.floor(uptime / (3600 * 24))}d ${Math.floor(uptime % (3600 * 24) / 3600)}h ${Math.floor(uptime % 3600 / 60)}m ${Math.floor(uptime % 60)}s` ,status: 200})
 }
 
 export default Status;
