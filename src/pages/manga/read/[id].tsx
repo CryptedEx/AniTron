9a762c41f8bfd212e07298b8c9be2f2027598832
Awe <106132059+Awe03@users.noreply.github.com>
2022-12-24 11:10:06 +0530

    Update [id].tsx

--- src/pages/manga/read/[id].tsx
+++ src/pages/manga/read/[id].tsx
@@ -4,7 +4,6 @@ import Manga from '@services/Manga/Manga';
 import {NextSeo} from 'next-seo';
 import {useRouter} from 'next/router';
 import {IMangaChapterPage, IMangaInfo} from '@consumet/extensions';
-import Image from 'next/image';
 import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 
@@ -16,33 +15,36 @@ const Read = ({manga}: {
     const router = useRouter();
     const imageRef = useRef<HTMLImageElement>();
 
-    const handlePage = (pg) => {
-        if (pg > data.length) return;
-        if (pg < 1) return;
-        setPage(pg);
+    const handlePage = (dir: string) => {
+        if (dir === 'next') {
+            if (page < data.length) {
+                setPage(page + 1);
+            } else if (+router.query.chap < manga.chapters.length + 1) router.push(`/manga/read/${manga.id}?chap=${+router.query.chap+1}`)
+        } else if (dir === 'back') {
+            if (page > 1) {
+                setPage(page - 1);
+            } else if (+router.query.chap > 1) router.push(`/manga/read/${manga.id}?chap=${+router.query.chap-1}`)
+        }
     }
 
-    //todo fix this
-
-    // useEffect(() => {
-    //     const handleClick = (e) => {
-    //         e.preventDefault();
-    //         if (e.target === imageRef.current) return;
-    //         if (e.clientX > window.innerWidth / 2) {
-    //             handlePage(page + 1);
-    //         }
-    //         if (e.clientX < window.innerWidth / 2) {
-    //             handlePage(page - 1);
-    //         }
-    //     }
-    //     window.addEventListener('click', handleClick);
-    //     //eslint-disable-next-line
-    // }, [page])
+    useEffect(() => {
+        const handleClick = (e) => {
+            e.preventDefault();
+            if (e.target !== imageRef.current) return;
+            if (e.clientX > window.innerWidth / 2) {
+                handlePage('next');
+            }
+            if (e.clientX < window.innerWidth / 2) {
+                handlePage('back');
+            }
+        }
+        window.addEventListener('click', handleClick);
+    }, [])
 
 
     useEffect(() => {
         (async () => {
-            const data = await Manga.getChapters(manga.chapters.find((chap) => parseInt(chap.id.split('-')[chap.id.split('-').length - 1]) === +router.query.chap).id);
+            const data = await Manga.getChapters(manga.chapters[manga.chapters.length-+router.query.chap]?.id);
             setData(data);
         })()
     }, [manga.chapters, router.query.chap])
@@ -50,9 +52,9 @@ const Read = ({manga}: {
     useEffect(() => {
         const handleArrow = (e) => {
             if (e.key === 'ArrowRight') {
-                handlePage(page + 1);
+                handlePage('next');
             } else if (e.key === 'ArrowLeft') {
-                handlePage(page - 1);
+                handlePage('back');
             }
         }
         window.addEventListener('keydown', handleArrow);
@@ -68,13 +70,6 @@ const Read = ({manga}: {
                 openGraph={{
                     images: [
                         {
-                            url: manga.image,
-                            type: 'small',
-                            alt: `${
-                                manga.title.english || manga.title.userPreferred || manga.title.romaji
-                            }`,
-                        },
-                        {
                             url: manga.cover || manga.image,
                             type: 'small',
                             alt: `Cover of ${
@@ -85,17 +80,34 @@ const Read = ({manga}: {
                 }}
             />
             <div
-                className={"flex flex-col items-center justify-center w-[100%] h-[100%] items-center align-center"} style={{marginTop: 'max(3rem, 3vw)'}}>
-                <div className={"w-full h-[2vh] bg-[#484747] flex justify-center"}>
-                    <div className={"text-white flex mt-1] inline"}>Chapter&nbsp;<div className={"text-[#A020F0]"}>{router.query.chap}</div>&nbsp;&nbsp;&nbsp;&nbsp;Page&nbsp;<div className={"text-[#A020F0]"}>{page}</div></div>
+                className={"flex flex-col items-center justify-center w-[100%] h-[100%] items-center align-center"} style={{marginTop: 'max(3rem, 3vh)'}}>
+                <div className={"w-full h-[2rem] bg-[#484747] flex justify-center"}>
+                    <div className={"text-white flex mt-[1] inline"}>Chapter&nbsp;<div className={"text-[#A020F0]"}>{manga.chapters[manga.chapters.length-+router.query.chap].id.split('-')[manga.chapters[manga.chapters.length-+router.query.chap].id.split('-').length-1]}</div>&nbsp;&nbsp;&nbsp;&nbsp;Page&nbsp;<div className={"text-[#A020F0]"}>{page}</div></div>
                 </div>
                 {data.length &&
-                    <img src={data.find((src) => +src.page === page).img} alt={'manga-image'}
-                           ref={imageRef} width={500} height={500}
-                           className={"w-[61.5vh] object-cover"}/>}
-                <div className={"w-full h-[2vh] bg-[#484747] flex justify-center"}>
-                    <KeyboardDoubleArrowLeftIcon className={"text-[2rem] cursor-pointer mr-[5rem]"} onClick={() => handlePage(page-1)}/>
-                    <KeyboardDoubleArrowRightIcon className={"text-[2rem] cursor-pointer ml-[5rem]"} onClick={() => handlePage(page+1)}/>
+                    <>
+                        <button
+                            className={"absolute z-10 bg-[#484747] text-white rounded-full w-[3rem] h-[3rem] flex justify-center items-center hidden md:flex"}
+                            style={{left: '0', top: '50%', transform: 'translateY(-50%)'}}
+                            onClick={() => handlePage('back')}
+                        >
+                            <KeyboardDoubleArrowLeftIcon/>
+                        </button>
+                        <button
+                            className={"absolute z-10 bg-[#484747] text-white rounded-full w-[3rem] h-[3rem] flex justify-center items-center hidden md:flex"}
+                            style={{right: '0', top: '50%', transform: 'translateY(-50%)'}}
+                            onClick={() => handlePage('next')}
+                        >
+                            <KeyboardDoubleArrowRightIcon/>
+                        </button>
+                        <img src={data.find((src) => +src.page === page).img} alt={'manga-image'}
+                             ref={imageRef} width={500} height={500}
+                             className={"w-[61.5vh] object-cover"}/>
+                    </>
+                }
+                <div className={"w-full h-[2rem] bg-[#484747] flex justify-center"}>
+                    <KeyboardDoubleArrowLeftIcon className={"text-[2rem] cursor-pointer mr-[5rem]"} onClick={() => handlePage('back')}/>
+                    <KeyboardDoubleArrowRightIcon className={"text-[2rem] cursor-pointer ml-[5rem]"} onClick={() => handlePage('next')}/>
                 </div>
             </div>
         </>
